1a99f3723b9bc1219bce047c0ecf9d9b37f5a4a6
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-06-12 20:14:00 -0700

    [Add] CMake Setup
    - Add CMake setup.
    - Implement C++ library to aid in calculations.
    - Create stubs to load image into C++.

--- pulsarfitpy/src/pulsars.h
+++
@@ -1,8 +0,0 @@
-#ifndef PULSARS_H
-#define PULSARS_H
-
-double add_numbers(double, double);
-double multiply_numbers(double, double);
-double divide_numbers(double, double);
-
-#endif // PULSARS_H
---
+++ pulsars/pulsars.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+#include "file_loader.h"
+
+int main(const int argc, char* argv[]) {
+	if (argc < 0) {
+		std::cerr << "Usage: pulsars <file location>\n";
+		std::cerr << "Example: pulsars /path/to/pulsars.png\n";
+
+		return -1;
+	}
+
+	std::string file_location = argv[1];
+
+	if (file_location.empty()) {
+		std::cerr << "Error: File location cannot be empty.\n";
+		return -1;
+	}
+
+	load_file(file_location);
+	std::vector<Point*> points = load_points(file_location);
+
+	return 0;
+}
