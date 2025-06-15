01c9104196103d02dcee06277a0dfe2b6bf6c4d3
Om-Kasar <contact.omkasar@gmail.com>
2025-06-15 14:14:20 -0700

    [Add] Polynomial Approximation Model
    - Edited labels
    - Outputted graphed polynomial approximation
    
    TODO: Create classes for generalized use

--- misc/mag-v-age.py
+++ misc/mag-v-age.py
@@ -24,7 +24,7 @@ pipeline = Pipeline([
 
 # Search for polynomial degree
 param_grid = {
-    'poly__degree': list(range(1, 20))  # Test degrees from 1 to 20
+    'poly__degree': list(range(1, 5))  # Test degrees from 1 to 5
 }
 
 # Integrates pipeline, tests degrees, and a grid fit for data
@@ -36,16 +36,26 @@ polynomial_model_X = np.linspace(ages.min(), ages.max(), 100).reshape(-1, 1) # I
 polynomial_model_Y = grid.best_estimator_.predict(polynomial_model_X)        # Generate approximate model
 
 # Print Degree
-degree = grid.best_params_['poly__degree']   # Output best degree
-print(f"Chosen Polynomial Degree: {degree}") # Print degree
+best_degree = grid.best_params_['poly__degree']     # Output best degree
+print(f"\nChosen Polynomial Degree: {best_degree}") # Print best degree
+
+coefficients = grid.best_estimator_.named_steps['reg'].coef_      # Gather coefficients into an array
+first_number = grid.best_estimator_.named_steps['reg'].intercept_ # Store first number of the polynomial
+
+# Build and print simplified polynomial function
+print("\nApproximated Polynomial Function:")
+print(f"f(x) = {first_number:.10f}", end='') # Prints each coefficient to 10 decimal places
+
+for i, coef in enumerate(coefficients[1:], start=1): # Extracts degree values (i) and coefficient number (coef) based on coefficients list indexing & enumerate
+    print(f" + {coef:.10f} * x**{i}", end='')        # Prints function in that order
 
 # Plot the approximated data points & polynomial regression function
 plt.figure(figsize=(10, 6))
 plt.scatter(ages, bsurfs, s=10, alpha=0.6, label='Pulsars')
-plt.plot(polynomial_model_X, polynomial_model_Y, color='red', label='Best Polynomial Fit') # Plot the model
+plt.plot(polynomial_model_X, polynomial_model_Y, color='red', label='Polynomial Approximation') # Plot the model
 plt.legend()
 plt.title('Observed Magnetic Field vs. Characteristic Age')
-plt.xlabel('log(Characteristic Age) [log(yrs)]')
-plt.ylabel('log(Magnetic Field) [log(B)]')
+plt.xlabel('Characteristic Age [log(yrs)]')
+plt.ylabel('Magnetic Field [log(G)]')
 plt.grid(True)
 plt.show()
